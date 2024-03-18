cfbff0adbe0bf2c40d24241ff632c70eea33fad9
Ryan Fitzer <ryan@ryanfitzer.com>
2024-03-18 12:23:09 -0700

    initial commit

---
+++ library/js/auth.js
@@ -0,0 +1,78 @@
+const handleAccountBtns = (client) => {
+  const createBtn = document.querySelector('[data-account-create]');
+  const loginBtn = document.querySelector('[data-account-login]');
+  const logoutBtn = document.querySelector('[data-account-logout]');
+
+  if (!client) return;
+
+  if (createBtn) {
+    createBtn.addEventListener('click', async () => {
+      await client.loginWithRedirect({
+        authorizationParams: {
+          screen_hint: 'signup',
+        },
+      });
+    });
+  }
+
+  if (loginBtn) {
+    loginBtn.addEventListener('click', async () => {
+      await client.loginWithRedirect();
+    });
+  }
+
+  if (logoutBtn) {
+    logoutBtn.addEventListener('click', async () => {
+      await client.logout({
+        clientId: 'oyxZtV6dvIpQ0ZSVmaJiwazo2s2u1782',
+        logoutParams: {
+          returnTo: window.auth.redirect,
+        },
+      });
+    });
+  }
+};
+
+const toggleAuthUI = (isLoggedIn) => {
+  if (isLoggedIn) document.documentElement.classList.add('auth');
+  else document.documentElement.classList.remove('auth');
+};
+
+const init = async () => {
+  let user;
+  let accessToken;
+  const url = new URLSearchParams(window.location.search);
+  const code = url.get('code');
+  const client = await window.auth0.createAuth0Client({
+    domain: 'dev-xaeh8ly873zk4166.us.auth0.com',
+    clientId: 'oyxZtV6dvIpQ0ZSVmaJiwazo2s2u1782',
+    authorizationParams: {
+      redirect_uri: window.auth.redirect,
+      audience: 'https://dev-xaeh8ly873zk4166.us.auth0.com/api/v2/',
+    },
+  });
+
+  if (code) {
+    await client.handleRedirectCallback();
+    window.history.replaceState(
+      {},
+      document.title,
+      window.location.origin + window.location.pathname
+    );
+  }
+
+  const isLoggedIn = await client.isAuthenticated();
+  console.log('isLoggedIn', isLoggedIn);
+  if (isLoggedIn) {
+    accessToken = await client.getTokenSilently();
+    user = await client.getUser();
+  }
+
+  console.log('user', user);
+
+  window.Webflow = window.Webflow || [];
+  window.Webflow.push(handleAccountBtns(client));
+  window.Webflow.push(toggleAuthUI(isLoggedIn));
+};
+
+init();
---
+++ library/js/swapi.js
@@ -0,0 +1,78 @@
+/* 
+level 0 - Show only the name
+level 1 - Add demographics including home world
+level 2 - Add vehicles and starships
+level 3 - Show all fields
+*/
+
+let levelQuery;
+const params = new URLSearchParams(window.location.search);
+const level = params.get('level');
+let props = {
+  0: 'name',
+  1: `homeworld {
+      name
+    }
+    birthYear
+    eyeColor
+    gender
+    hairColor
+    height
+    mass
+    skinColor
+    species {
+      name
+    }`,
+  2: `starshipConnection {
+        starships {
+          name
+        }
+      }
+      vehicleConnection {
+        vehicles {
+          name
+        }
+      }`,
+  3: `filmConnection {
+        films {
+          title
+        }
+      }`,
+};
+
+if (level === '0') {
+  levelQuery = props['0'];
+}
+
+if (level === '1') {
+  levelQuery = `${props['0']}\n${props['1']}`;
+}
+
+if (level === '2') {
+  levelQuery = `${props['0']}\n${props['1']}\n${props['2']}`;
+}
+
+if (level === '3') {
+  levelQuery = `${props['0']}\n${props['1']}\n${props['2']}\n${props['3']}`;
+}
+
+const finalQuery = `query Query($personID: ID) {
+  person(personID: $personID) {
+    ${levelQuery}
+  }
+}`;
+
+const runQuery = () => {
+  fetch('https://swapi-graphql.netlify.app/.netlify/functions/index', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ query: finalQuery, variables: { personID: '13' } }),
+  })
+    .then((r) => r.json())
+    .then((data) => console.log('data returned:', data.data));
+};
+
+window.runQuery = runQuery;
